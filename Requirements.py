94dba9eb42e0348cf75e96f79f6b90f5d7bdd312
AmitKehrwal <144267580+AmitKehrwal@users.noreply.github.com>
2023-12-09 13:25:07 +0530

    Update Requirements.py

--- Requirements.py
+++ Requirements.py
@@ -15,11 +15,32 @@ os.system('wget -qO- https://deb.opera.com/archive.key | apt-key add - > /dev/nu
 # Set DEBIAN_FRONTEND to non-interactive mode
 os.environ['DEBIAN_FRONTEND'] = 'noninteractive'
 
-# Add Opera repository
-os.system('add-apt-repository -y "deb [arch=i386,amd64] https://deb.opera.com/opera-stable/ stable non-free" > /dev/null 2>&1')
+import subprocess
 
-# Install Opera
-os.system('apt install -y opera-stable > /dev/null 2>&1')
+# Install wget
+subprocess.run(["apt", "install", "wget", "-y"])
+
+# Install webdriver_manager
+subprocess.run(["pip", "install", "webdriver_manager"])
+
+# Upgrade webdriver_manager
+subprocess.run(["pip", "install", "--upgrade", "webdriver_manager"])
+
+# Install curl
+subprocess.run(["apt", "install", "curl", "-y"])
+
+# Download Brave keyring
+subprocess.run(["curl", "-fsSLo", "/usr/share/keyrings/brave-browser-archive-keyring.gpg", "https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg"])
+
+# Add Brave repository
+subprocess.run(["echo", "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main", "|", "sudo", "tee", "/etc/apt/sources.list.d/brave-browser-release.list"])
+
+# Update package list
+subprocess.run(["apt-get", "update"])
+
+# Install Brave browser
+subprocess.run(["apt", "install", "brave-browser"])
+
 
 # Install selenium version 4.2.0
 os.system('pip install selenium==4.2.0 > /dev/null 2>&1')
